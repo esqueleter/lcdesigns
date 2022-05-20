156601c0b7935a2f7c7fc390ad487596edf6729c
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-20 17:51:06 -0300

    feat: Logo navbar

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Navbar } from 'react-bootstrap'
+import logo from "../../assets/images/reactlogo.png"
 
 const Navigation: NextPage = () => {
   return (
    <>
-      <Navbar bg="dark">
-
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <Image src={logo} layout="fill" alt="Logo"/>
+        </Navbar.Brand>
       </Navbar>
    </>
   )
