870c08cb534c6a368b117837ff6ff909c4834f3b
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-23 17:56:21 -0300

    feat: bootstrap to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.css"
+import '../styles/globals.css'
 import Navigation from '../components/Navigation'
+import { SSRProvider } from 'react-bootstrap'
 import type { AppProps } from 'next/app'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Navigation/>
-    <Component {...pageProps} />
+    <SSRProvider>
+      <Navigation/>
+      <Component {...pageProps} />
+    </SSRProvider>
   </>
  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Container from '../components/Container'
 
 const Home: NextPage = () => {
   return (
    <>
-     
+     <Container/>
    </>
   )
 }
