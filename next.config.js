65b00178eb6c1cb205f197b9badc96080ad58355
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-23 17:58:02 -0300

    feat: new dev settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { 
+    domains: ['static.tumblr.com', "img001.prntscr.com"], 
+    formats: ['image/avif', 'image/webp'] 
+  }
 }
 
 module.exports = nextConfig
