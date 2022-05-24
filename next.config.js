4ab4ff2ab03976a4c809ca677c22a10e0344d69c
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:39:58 -0300

    URL permissions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { 
-    domains: ['static.tumblr.com', "img001.prntscr.com"], 
+    domains: ['static.tumblr.com', "img001.prntscr.com", "avatars.githubusercontent.com"], 
     formats: ['image/avif', 'image/webp'] 
   }
 }
