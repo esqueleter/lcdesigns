9c2a8b750625dbf09a9cc66b028268c0076bb4d2
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:37:52 -0300

    feat: navigation on all pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import Container from '../components/Container'
 
+
 const Home: NextPage = () => {
   return (
    <>
-     <Container/>
+      <Container/>
    </>
   )
 }
