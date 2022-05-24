6537774a7c07854c4df06524d2a0d3101630df4d
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:33:32 -0300

    feat: New components added

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,11 +1,18 @@
-import type { NextPage } from 'next'
-import Welcome from "../Welcome"
-import Projects from '../Projects'
+import type { NextPage } from 'next';
+import Welcome from "../Welcome";
+import Projects from '../Projects';
+import ScrollTop from '../ScrollTop';
+import AboutMe from '../AboutMe';
+import AboutEnterprise from '../AboutEnterprise';
+
 const Container: NextPage = () => {
   return (
    <>
       <Welcome/>
       <Projects/>
+      <AboutMe/>
+      <AboutEnterprise/>
+      <ScrollTop/>
    </>
   )
 }
