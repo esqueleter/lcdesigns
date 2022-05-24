22e1896ae55f29d121e2f9feaf4a2cd7ed77ab73
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:35:35 -0300

    navigation new offsets

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image'
 import {Link} from "react-scroll"
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import logo from "../../assets/images/reactlogo.svg"
+import styles from "./styles.module.scss"
 
 const Navigation: NextPage = () => {
   return (
    <>
-      <Navbar bg="dark" variant="dark" sticky="top" expand="lg" className="px-3">
+      <Navbar bg="dark" variant="dark" expand="lg" className={`${styles.navigation} px-3`}>
         <Navbar.Brand className="d-flex align-items-center">
           <Image src={logo} width="40px" height="40px" alt="Logo" />
           <span className="ms-3">LCDesigns</span>
@@ -18,29 +19,27 @@ const Navigation: NextPage = () => {
         <Navbar.Collapse className="justify-content-end me-4">
           <Nav>
               <Nav.Link as="span">
-                <Link to="home" spy={true} smooth={true} offset={0} duration={300}>Ínicio</Link>
+                <Link to="home" spy={true} smooth={true} offset={-100} duration={200}>Ínicio</Link>
               </Nav.Link>
           
               <Nav.Link as="span">
-                <Link to="projects" spy={false} smooth={true} offset={100} duration={300}>Projetos</Link>
+                <Link to="projects" spy={false} smooth={true} offset={-40} duration={200}>Projetos</Link>
               </Nav.Link>
 
-              <Nav.Link as="span">
-                <Link to="collections" spy={true} smooth={true} offset={50} duration={300}>Coleções</Link>
-              </Nav.Link>
-          
+              <Nav.Link href="/colletions">Coleções</Nav.Link>
+
               <NavDropdown title="Sobre">
                 <NavDropdown.Item>
-                  <Link to="about-me" spy={true} smooth={true} offset={50} duration={300}>Sobre a Designer</Link>
+                  <Link to="about-me" spy={true} smooth={true} offset={-40} duration={200}>Sobre a Designer</Link>
                 </NavDropdown.Item>
 
                 <NavDropdown.Item>
-                  <Link to="about-enterprise" spy={true} smooth={true} offset={50} duration={300}>Sobre LCDesingns</Link>
+                  <Link to="about-enterprise" spy={true} smooth={true} offset={50} duration={200}>Sobre LCDesingns</Link>
                 </NavDropdown.Item>
               </NavDropdown>
           
               <Nav.Link as="span">
-               <Link to="contact" spy={true} smooth={true} offset={50} duration={300}>Contato</Link>
+               <Link to="contact" spy={true} smooth={true} offset={50} duration={200}>Contato</Link>
               </Nav.Link>
           </Nav>
         </Navbar.Collapse>
