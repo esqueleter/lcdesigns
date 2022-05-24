131033a08e54ccc8f7a6bac45e1435fdd0405845
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:36:31 -0300

    welcome offset button adjust

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -8,14 +8,14 @@ import {Link} from "react-scroll"
 const Welcome: NextPage = () => {
   return (
    <>
-        <Container id="home" fluid className={`${styles.welcome} min-vh-100 container-xxl`} >
-          <Stack gap={4} className="d-flex col-md-4 mx-auto justify-content-center align-items-center vh-100"> 
+        <Container id="home" fluid className={styles.welcome} >
+          <Stack gap={5} className={`${styles.stack} d-flex col-md-4 mx-auto justify-content-center align-items-center`}> 
           
             <Image src="https://static.tumblr.com/anxftez/F4tq7ucmf/ezgif-5-4c3c2420295d.gif" alt="Logo-gif" width={350} height={350}/>
 
             <h3 className="text-white">Design text wrapper</h3>
 
-            <Link to="projects" spy={false} smooth={true} offset={100} duration={300}>
+            <Link to="projects" spy={false} smooth={true} offset={-40} duration={200}>
               <Image src={arrowDown} width="45px" height="45px" alt="" className={styles.arrowDown}/>
             </Link>
 
