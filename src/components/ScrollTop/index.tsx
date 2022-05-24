6566388b4eb0f6c5baa5fc30d1c3d0b982dd67e4
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-05-24 17:32:42 -0300

    feat: ScrollTop button

---
+++ src/components/ScrollTop/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import {Link} from "react-scroll"
+import arrowScrollTop from "../../assets/icons/arrowScrollTop.svg"
+import styles from "./styles.module.scss"
+
+const ScrollTop: NextPage = () => {
+  return (
+   <>
+        <Link 
+            to="home" 
+            spy={true} 
+            smooth={true} 
+            offset={-100} 
+            duration={200}
+            className={`${styles.scrollTop} rounded-circle bg-black m-3`}
+            >
+            <Image src={arrowScrollTop} alt="Scroll Top" layout="fill"/>
+        </Link>
+        
+   </>
+  )
+}
+
+export default ScrollTop
