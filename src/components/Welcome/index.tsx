9602faacaaa299ebb9a73e1b527999c22bb9e968
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-06-03 18:04:45 -0300

    feat: Layout added

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,28 +1,47 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import {Container, Stack} from "react-bootstrap"
-import styles from "./styles.module.scss"
-import arrowDown from "../../assets/icons/arrowDown.svg"
-import {Link} from "react-scroll"
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import { Container, Stack } from 'react-bootstrap';
+import styles from './styles.module.scss';
+import arrowDown from '../../assets/icons/arrowDown.svg';
+import { Link } from 'react-scroll';
+import Layout from '../Layout';
 
 const Welcome: NextPage = () => {
   return (
-   <>
-        <Container id="home" fluid className={styles.welcome} >
-          <Stack gap={5} className={`${styles.stack} d-flex col-md-4 mx-auto justify-content-center align-items-center`}> 
-          
-            <Image src="https://static.tumblr.com/anxftez/F4tq7ucmf/ezgif-5-4c3c2420295d.gif" alt="Logo-gif" width={350} height={350}/>
+    <Layout>
+      <Container id="home" fluid className={styles.welcome}>
+        <Stack
+          gap={5}
+          className={`${styles.stack} d-flex col-md-4 mx-auto mt-5 justify-content-center align-items-center`}
+        >
+          <Image
+            src="https://static.tumblr.com/anxftez/F4tq7ucmf/ezgif-5-4c3c2420295d.gif"
+            alt="Logo-gif"
+            width={350}
+            height={350}
+          />
 
-            <h3 className="text-white">Design text wrapper</h3>
+          <h3 className="text-white">Design text wrapper</h3>
 
-            <Link to="projects" spy={false} smooth={true} offset={-40} duration={200}>
-              <Image src={arrowDown} width="45px" height="45px" alt="" className={styles.arrowDown}/>
-            </Link>
+          <Link
+            to="projects"
+            spy={false}
+            smooth={true}
+            offset={-40}
+            duration={200}
+          >
+            <Image
+              src={arrowDown}
+              width="45px"
+              height="45px"
+              alt=""
+              className={styles.arrowDown}
+            />
+          </Link>
+        </Stack>
+      </Container>
+    </Layout>
+  );
+};
 
-          </Stack>
-        </Container>
-   </>
-  )
-}
-
-export default Welcome
+export default Welcome;
