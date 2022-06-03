b355c3e911dc3b742b4a4466f2282b3a9a9f4405
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-06-03 18:04:20 -0300

    feat: Navigation in index; globals no changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,15 @@
-import "bootstrap/dist/css/bootstrap.css"
-import '../styles/globals.css'
-import Navigation from '../components/Navigation'
-import { SSRProvider } from 'react-bootstrap'
-import type { AppProps } from 'next/app'
-
-
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/globals.css';
+import { SSRProvider } from 'react-bootstrap';
+import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <SSRProvider>
-      <Navigation/>
-      <Component {...pageProps} />
-    </SSRProvider>
-  </>
- )
+    <>
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,71 @@
-import type { NextPage } from 'next'
-import Container from '../components/Container'
-
-
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import Welcome from '../components/Welcome';
+import Projects from '../components/Projects';
+import ScrollTop from '../components/ScrollTop';
+import AboutMe from '../components/AboutMe';
+import Navigation from '../components/Navigation';
+import AboutEnterprise from '../components/AboutEnterprise';
 const Home: NextPage = () => {
-  return (
-   <>
-      <Container/>
-   </>
-  )
-}
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [scrollBarState, setScrollBarState] = useState(false);
 
-export default Home
+  let doc = document.documentElement;
+  let w = window;
+
+  let prevScroll = w.scrollY || doc.scrollTop;
+  let curScroll;
+  let direction = 0;
+  let prevDirection = 0;
+
+  const controlNavbar = () => {
+    const toggleHeader = (direction: number, curScroll: number) => {
+      if (direction === 2 && curScroll > 100) {
+        setShowNavbar(false);
+        setScrollBarState(true);
+        prevDirection = direction;
+      } else if (direction === 1) {
+        setShowNavbar(true);
+        setScrollBarState(false);
+        prevDirection = direction;
+      }
+    };
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      direction = 2;
+    } else if (curScroll < prevScroll) {
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+    prevScroll = curScroll;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Navigation navbarState={showNavbar} />
+      <Welcome />
+
+      <Projects />
+
+      <AboutMe />
+
+      <AboutEnterprise />
+
+      <ScrollTop scrollBarState={scrollBarState} />
+    </>
+  );
+};
+
+export default Home;
