522de7bcfaa96c4885b9271c22881f5939b31093
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-06-03 18:02:01 -0300

    feat: Layout added

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,28 +1,32 @@
-import type { NextPage } from 'next'
-import {Container, Stack, Button} from "react-bootstrap"
-import ImageCard from '../ImageCard'
-import Link from "next/link"
+import type { NextPage } from 'next';
+import { Container, Stack, Button } from 'react-bootstrap';
+import ImageCard from '../ImageCard';
+import Link from 'next/link';
+import Layout from '../Layout';
 
 const Projects: NextPage = () => {
   return (
-   <>
-        <Container id="projects" fluid>
-            <h3 className="d-flex justify-content-center my-5">Projetos</h3>
-            <Stack className="d-flex flex-wrap my-3 justify-content-center" direction='horizontal' gap={5}>
-              <ImageCard/>
-              <ImageCard/>
-              <ImageCard/>  
-              <ImageCard/>
-              <ImageCard/>
-              <ImageCard/>  
-            </Stack>
-            <Button className="d-flex mx-auto my-4">
-              <Link href="/collections">Ver todos os projetos</Link>
-            </Button>
-            
-        </Container>
-   </>
-  )
-}
+    <Layout>
+      <Container id="projects" fluid>
+        <h3 className="d-flex justify-content-center my-5">Projetos</h3>
+        <Stack
+          className="d-flex flex-wrap my-3 justify-content-center"
+          direction="horizontal"
+          gap={5}
+        >
+          <ImageCard />
+          <ImageCard />
+          <ImageCard />
+          <ImageCard />
+          <ImageCard />
+          <ImageCard />
+        </Stack>
+        <Button className="d-flex mx-auto my-4">
+          <Link href="/collections">Ver todos os projetos</Link>
+        </Button>
+      </Container>
+    </Layout>
+  );
+};
 
-export default Projects
+export default Projects;
