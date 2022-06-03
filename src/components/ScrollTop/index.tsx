a90fdae8f2c1b7a9d17499c0832f702e3634730f
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2022-06-03 18:03:16 -0300

    feat: ScrollTop fade

--- src/components/ScrollTop/index.tsx
+++ src/components/ScrollTop/index.tsx
@@ -1,25 +1,28 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import {Link} from "react-scroll"
-import arrowScrollTop from "../../assets/icons/arrowScrollTop.svg"
-import styles from "./styles.module.scss"
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import { Link } from 'react-scroll';
+import arrowScrollTop from '../../assets/icons/arrowScrollTop.svg';
+import styles from './styles.module.scss';
 
-const ScrollTop: NextPage = () => {
+const ScrollTop: NextPage<{ scrollBarState: boolean }> = ({
+  scrollBarState,
+}) => {
   return (
-   <>
-        <Link 
-            to="home" 
-            spy={true} 
-            smooth={true} 
-            offset={-100} 
-            duration={200}
-            className={`${styles.scrollTop} rounded-circle bg-black m-3`}
-            >
-            <Image src={arrowScrollTop} alt="Scroll Top" layout="fill"/>
-        </Link>
-        
-   </>
-  )
-}
+    <>
+      <Link
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={200}
+        className={`${styles.scrollTop} ${
+          scrollBarState && styles.on
+        } rounded-circle bg-black m-3`}
+      >
+        <Image src={arrowScrollTop} alt="Scroll Top" layout="fill" />
+      </Link>
+    </>
+  );
+};
 
-export default ScrollTop
+export default ScrollTop;
